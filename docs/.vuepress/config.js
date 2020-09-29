1a71131dcd9b27a6a4ab91c9f41b7840deba05b3
kiyeoneee <kyeoneee@gmail.com>
2020-09-30 01:02:46 +0900

    [20200930] add pages sidebar of clean code directory

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,32 +1,29 @@
-const { getFilesOf } = require('./files_util.js');
+const SIDEBAR_CONST = require('./sidebar-const');
 
 module.exports = {
   title: 'Today kiyeoneee Learned',
   description: 'What kiyeoneee learned everyday',
   base: '/TIL/',
   themeConfig: {
+    search: true,
+    sidebar: [
+      {
+        title: 'Books',
+        children: [
+          {
+            title: 'Clean Code',
+            path: '/book/clean-code',
+            children: SIDEBAR_CONST.CleanCode
+          }
+        ]
+      }
+    ],
     nav: [
       { text: 'Github', link: 'https://github.com/kiyeoneee' }, 
       { text: 'Tech Blog', link: 'https://kyeoneee.tistory.com/' }
     ],
-    sideBar: [
-    ]
   },
   repo: 'kiyeoneee/TIL',
   repoLabel: 'GitHub',
-  editLinks: true,
-  plugins: {
-    "vuepress-plugin-auto-sidebar": {
-      titleMap: {
-        "book": "Books",
-        "elasticsearch": "ElasticSearch",
-        "etc": "ETC",
-        "languages": "Languages",
-        "linux": "Linux",
-        "system": "System",
-        "toy-project": "Toy Projects",
-        misc: "Miscellany"
-      }
-    },
-  }
+  editLinks: true
 }
